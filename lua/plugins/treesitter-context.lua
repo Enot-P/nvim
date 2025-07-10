bf35fb81f18c911f6740e3d173b1d9c84e096d68
Enot <nikita.ptashkin2@gmail.com>
2025-07-10 20:58:32 +0300

    fix

--- lua/plugins/treesitter-context.lua
+++ lua/plugins/treesitter-context.lua
@@ -2,7 +2,8 @@ return {
 	"nvim-treesitter/nvim-treesitter-context",
 	config = function()
 		require("treesitter-context").setup({
-			enable = false,
+			enable = true,
+			max_lines = 0,
 		})
 	end,
 }
