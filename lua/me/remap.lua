20c9be2e6edf04c7ddfcaf02c6eef698a228b2b6
Nikita Ptashkin <nikita.ptashckin@yandex.ru>
2025-08-06 21:46:34 +0300

    Добавил сворачивание

--- lua/me/remap.lua
+++ lua/me/remap.lua
@@ -33,7 +33,7 @@ vim.keymap.set("n", "<A-k>", ":resize -2<CR>", { desc = "Уменьшить вы
 vim.keymap.set("n", "<A-h>", ":vertical resize +2<CR>", { desc = "Увеличить ширину окна" })
 vim.keymap.set("n", "<A-l>", ":vertical resize -2<CR>", { desc = "Уменьшить ширину окна" })
 vim.keymap.set("n", "<leader>dl", ":Telescope diagnostics bufnr=0<CR>", { desc = "Показать список ошибок" })
-vim.keymap.set("n", "<leader>df", ":Telescope diagnostics bufnr=nil<CR>", { desc = "Показать ошибки проекта" })
+vim.keymap.set("n", "<leader>dp", ":Telescope diagnostics bufnr=nil<CR>", { desc = "Показать ошибки проекта" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Показать ошибку под курсором" })
 vim.keymap.set("n", "<leader>ge", function()
   require("me.exports").generate_exports()
--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -6,8 +6,22 @@ return {
     vim.o.timeoutlen = 300
   end,
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    plugins = {
+      marks = true,
+      registers = true,
+      spelling = {
+        enabled = true,
+        suggestions = 20,
+      },
+      presets = {
+        operators = false,
+        motions = false,
+        text_objects = false,
+        windows = false,
+        nav = false,
+        z = true, -- enable z prefix for folds
+        g = false,
+      },
+    },
   }
 }
