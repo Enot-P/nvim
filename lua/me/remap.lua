0b00747efcf0a726aae4d50acb7f66beec89c374
Nikita_Ptashkin <nikita.ptashckin@yandex.ru>
2025-08-07 04:31:46 +0300

    NeoTree открывает на текущем файле

--- lua/me/remap.lua
+++ lua/me/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>q", ":Neotree toggle<CR>", { desc = "Переключить NeoTree" })
+vim.keymap.set("n", "<leader>e", ":Neotree reveal<CR>", { desc = "Переключить NeoTree" })
 vim.keymap.set("n", "<leader>|", "<cmd>vsplit<CR>", { desc = "Вертикальное разделение" })
 vim.keymap.set("n", "<leader>\\", "<cmd>split<CR>", { desc = "Горизонтальное разделение" })
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Перейти в левое окно" })
@@ -12,7 +12,7 @@ vim.keymap.set("n", "<leader>Q", ":q!<CR>", { desc = "Выйти без сохр
 vim.keymap.set("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>", { desc = "Следующий буфер" })
 vim.keymap.set("n", "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", { desc = "Предыдущий буфер" })
 vim.keymap.set("n", "<leader>tp", ":Telescope projects<CR>", { desc = "Поиск проектов" })
-vim.keymap.set("n", "<leader>bc", ":bdelete<CR>", { desc = "Закрыть текущий буфер" })
+vim.keymap.set("n", "<leader>q", ":bdelete<CR>", { desc = "Закрыть текущий буфер" })
 for i = 1, 9 do
 	vim.keymap.set(
 		"n",
@@ -34,7 +34,7 @@ vim.keymap.set("n", "<A-h>", ":vertical resize +2<CR>", { desc = "Увеличи
 vim.keymap.set("n", "<A-l>", ":vertical resize -2<CR>", { desc = "Уменьшить ширину окна" })
 vim.keymap.set("n", "<leader>dl", ":Telescope diagnostics bufnr=0<CR>", { desc = "Показать список ошибок" })
 vim.keymap.set("n", "<leader>dp", ":Telescope diagnostics bufnr=nil<CR>", { desc = "Показать ошибки проекта" })
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Показать ошибку под курсором" })
+vim.keymap.set("n", "<leader>dj", vim.diagnostic.open_float, { desc = "Показать ошибку под курсором" })
 vim.keymap.set("n", "<leader>ge", function()
   require("me.exports").generate_exports()
 end, { desc = "Сгенерировать экспортный файл" })
