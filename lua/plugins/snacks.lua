23957c89917f5bd65b6ccdc003206c04632e9541
Nikita Ptashkin <nikita.ptashckin@yandex.ru>
2025-08-09 16:13:02 +0300

    snacks настроил

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -29,7 +29,7 @@ return {
           enabled = false,          -- Отключить отображение областей как кусков
           only_current = false,     -- Показывать куски во всех окнах
           priority = 200,           -- Приоритет кусков
-          hl = "SnacksIndentChunk", -- Групsturdy, -- Группа подсветки для кусков
+          hl = "SnacksIndentChunk", -- Группа подсветки для кусков
           char = {
             corner_top = "┌",
             corner_bottom = "└",
@@ -40,6 +40,31 @@ return {
         },
         filter = function(buf)
           return vim.g.snacks_indent ~= false and vim.b[buf].snacks_indent ~= false and vim.bo[buf].buftype == ""
+        end,
+      },
+      statuscolumn = {
+        enabled = true,            -- Включить кастомную строку состояния
+        left = { "mark", "sign" }, -- Приоритет компонентов слева (высокий к низкому)
+        right = { "fold", "git" }, -- Приоритет компонентов справа (высокий к низкому)
+        folds = {
+          open = false,            -- Не показывать иконки открытых складок
+          git_hl = false,          -- Не использовать подсветку Git Signs для иконок складок
+        },
+        git = {
+          patterns = { "GitSign", "MiniDiffSign" }, -- Шаблоны для Git-знаков
+        },
+        refresh = 50,                               -- Обновление каждые 50 мс
+      },
+      words = {
+        enabled = true,            -- Включить функцию words
+        debounce = 200,            -- Задержка перед обновлением (мс)
+        notify_jump = false,       -- Не показывать уведомления при прыжке
+        notify_end = true,         -- Показывать уведомление при достижении конца списка ссылок
+        foldopen = true,           -- Открывать складки при прыжке
+        jumplist = true,           -- Добавлять точку прыжка в jumplist
+        modes = { "n", "i", "c" }, -- Режимы, в которых показывать ссылки
+        filter = function(buf)
+          return vim.g.snacks_words ~= false and vim.b[buf].snacks_words ~= false
         end,
       },
     },
