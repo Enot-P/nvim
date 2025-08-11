1b53f00aacf15838fc144822221bc25dd8930055
Nikita_Ptashkin <nikita.ptashckin@yandex.ru>
2025-08-11 03:51:32 +0300

    debug стартовый конфигурация добавлена
    Неудобно что нужно указывать сервис этот vm

--- lua/plugins/flutter_tools.lua
+++ lua/plugins/flutter_tools.lua
@@ -20,7 +20,21 @@ return {
           }
         },
         debugger = {
-          enabled = false,
+          enabled = true, -- Включаем отладчик
+          run_via_dap = true,
+          register_configurations = function(paths)
+            return {
+              {
+                name = "Launch Flutter (New Process)",
+                type = "dart",
+                request = "launch",
+                program = "${workspaceFolder}/lib/main.dart",
+                cwd = "${workspaceFolder}",
+                args = {},
+                toolArgs = {},
+              }
+            }
+          end,
         },
         flutter_path = nil, -- Автоматическое определение пути
         flutter_lookup_cmd = nil,
@@ -75,6 +89,192 @@ return {
           }
         }
       })
+
+      -- Дополнительные функции для работы с отладкой
+      local function attach_to_running_flutter()
+        -- Пытаемся автоматически найти VM Service URI из tmux окна
+        local handle = io.popen("tmux capture-pane -t 1 -p | grep 'A Dart VM Service' | tail -1")
+        if handle then
+          local output = handle:read("*a")
+          handle:close()
+
+          local uri = output:match("http://[%d%.]+:%d+/[%w%-_]+=/")
+          if uri then
+            print("Найден VM Service URI: " .. uri)
+            -- Подключаемся к отладчику через DAP
+            require('dap').run({
+              type = 'dart',
+              request = 'attach',
+              name = 'Attach to Running Flutter',
+              vmServiceUri = uri,
+            })
+          else
+            print("Не удалось найти VM Service URI автоматически")
+            local manual_uri = vim.fn.input("Введите VM Service URI: ", "http://127.0.0.1:34499/")
+            if manual_uri and manual_uri ~= "" then
+              require('dap').run({
+                type = 'dart',
+                request = 'attach',
+                name = 'Attach to Running Flutter',
+                vmServiceUri = manual_uri,
+              })
+            end
+          end
+        end
+      end
+
+      local function get_flutter_vm_service_info()
+        local handle = io.popen("tmux capture-pane -t 1 -p | grep -E '(A Dart VM Service|Flutter DevTools)' | tail -2")
+        if handle then
+          local output = handle:read("*a")
+          handle:close()
+          print("Flutter Debug Info:")
+          print(output)
+
+          -- Показываем также активные DAP сессии
+          local dap = require('dap')
+          if dap.session() then
+            print("DAP Session: Active")
+          else
+            print("DAP Session: Not active")
+          end
+        else
+          print("Не удалось получить информацию о Flutter процессе")
+        end
+      end
+
+      -- Команды для удобной работы
+      vim.api.nvim_create_user_command("FlutterAttach", attach_to_running_flutter, {
+        desc = "Attach to running Flutter process"
+      })
+
+      vim.api.nvim_create_user_command("FlutterDebugInfo", get_flutter_vm_service_info, {
+        desc = "Show Flutter VM Service information"
+      })
+
+      -- Keymaps для отладки
+      vim.keymap.set('n', '<leader>fa', attach_to_running_flutter, { desc = 'Attach to Flutter process' })
+      vim.keymap.set('n', '<leader>fd', get_flutter_vm_service_info, { desc = 'Flutter debug info' })
+
+      -- Стандартные DAP keymaps
+      vim.keymap.set('n', '<leader>db', require('dap').toggle_breakpoint, { desc = 'Toggle breakpoint' })
+      vim.keymap.set('n', '<leader>dc', require('dap').continue, { desc = 'Continue' })
+      vim.keymap.set('n', '<leader>dn', require('dap').step_over, { desc = 'Step over' })
+      vim.keymap.set('n', '<leader>di', require('dap').step_into, { desc = 'Step into' })
+      vim.keymap.set('n', '<leader>do', require('dap').step_out, { desc = 'Step out' })
+      vim.keymap.set('n', '<leader>dr', require('dap').repl.open, { desc = 'Open REPL' })
+    end
+  },
+  -- Добавляем nvim-dap для отладки
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'rcarriga/nvim-dap-ui',
+      'nvim-neotest/nvim-nio',
+    },
+    config = function()
+      local dap = require('dap')
+      local dapui = require('dapui')
+
+      -- Настройка DAP адаптера для Dart
+      dap.adapters.dart = {
+        type = 'executable',
+        command = 'dart',
+        args = { 'debug_adapter' }
+      }
+
+      -- Альтернативная настройка адаптера (если первая не работает)
+      dap.adapters.flutter = {
+        type = 'executable',
+        command = 'flutter',
+        args = { 'debug_adapter' }
+      }
+
+      -- Конфигурации для запуска/подключения
+      dap.configurations.dart = {
+        {
+          type = "dart",
+          request = "launch",
+          name = "Launch Flutter",
+          dartSdkPath = vim.fn.system('which dart'):gsub('\n', ''):gsub('/bin/dart', ''),
+          flutterSdkPath = vim.fn.system('which flutter'):gsub('\n', ''):gsub('/bin/flutter', ''),
+          program = "${workspaceFolder}/lib/main.dart",
+          cwd = "${workspaceFolder}",
+        },
+        {
+          type = "dart",
+          request = "attach",
+          name = "Attach to process",
+          vmServiceUri = function()
+            return vim.fn.input('VM Service URI: ', 'http://127.0.0.1:34499/')
+          end,
+        }
+      }
+
+      -- Настройка dapui
+      dapui.setup({
+        icons = { expanded = "▾", collapsed = "▸" },
+        mappings = {
+          -- Use a table to apply multiple mappings
+          expand = { "<CR>", "<2-LeftMouse>" },
+          open = "o",
+          remove = "d",
+          edit = "e",
+          repl = "r",
+          toggle = "t",
+        },
+        layouts = {
+          {
+            elements = {
+              -- Elements can be strings or table with id and size keys.
+              { id = "scopes", size = 0.25 },
+              "breakpoints",
+              "stacks",
+              "watches",
+            },
+            size = 40, -- 40 columns
+            position = "left",
+          },
+          {
+            elements = {
+              "repl",
+              "console",
+            },
+            size = 0.25, -- 25% of total lines
+            position = "bottom",
+          },
+        },
+        floating = {
+          max_height = nil,  -- These can be integers or a float between 0 and 1.
+          max_width = nil,   -- Floats will be treated as percentage of your screen.
+          border = "single", -- Border style. Can be "single", "double" or "rounded"
+          mappings = {
+            close = { "q", "<Esc>" },
+          },
+        },
+        windows = { indent = 1 },
+        render = {
+          max_type_length = nil, -- Can be integer or nil.
+        }
+      })
+
+      -- Автоматически открываем/закрываем UI
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+
+      -- Keymap для UI
+      vim.keymap.set('n', '<leader>du', dapui.toggle, { desc = 'Toggle DAP UI' })
+
+      -- Дополнительные keymaps для управления сессией отладки
+      vim.keymap.set('n', '<leader>ds', dap.terminate, { desc = 'Stop debugging session' })
+      vim.keymap.set('n', '<leader>dR', dap.restart, { desc = 'Restart debugging session' })
     end
   }
 }
--- lua/plugins/tmux-runner.lua
+++ lua/plugins/tmux-runner.lua
@@ -4,29 +4,29 @@ return {
     local function send_key_to_flutter_window(key)
       -- Получаем информацию о текущем окне
       local current_window = vim.fn.system("tmux display-message -p '#I'"):gsub("\n", "")
-      
+
       -- Получаем список всех окон
       local windows_info = vim.fn.system("tmux list-windows -F '#{window_index} #{window_active} #{window_name}'")
-      
+
       local target_window = nil
-      
+
       -- Ищем окна и определяем целевое
       for line in windows_info:gmatch("[^\r\n]+") do
         local win_index, is_active, win_name = line:match("([0-9]+)%s+([01])%s+(.*)")
-        
+
         if win_index and is_active == "0" then
           --Берем первое неактивное окно как целевое
           if not target_window then
             target_window = win_index
           end
-          
+
           -- Или ищем окно с Flutter (если есть характерные названия)
           if win_name and (win_name:match("[Ff]lutter") or win_name:match("[Dd]art")) then
             target_window = win_index
           end
         end
       end
-      
+
       -- Отправляем команду в целевое окно
       if target_window then
         local cmd = string.format("tmux send-keys -t %s %s Enter", target_window, key)
@@ -35,7 +35,7 @@ return {
         -- Пробуем отправить в следующее окно
         local next_window = tonumber(current_window) + 1
         local cmd = string.format("tmux send-keys -t %s %s Enter 2>/dev/null", next_window, key)
-        
+
         if vim.v.shell_error ~= 0 then
           -- Пробуем предыдущее окно
           local prev_window = tonumber(current_window) - 1
@@ -50,19 +50,82 @@ return {
     local function dart_hot_reload()
       send_key_to_flutter_window("'r'")
     end
-    
+
+    -- Функция для получения информации о Flutter процессе из tmux
+    local function get_flutter_process_info()
+      local handle = io.popen("tmux capture-pane -t 1 -p | grep -A5 -B5 'A Dart VM Service'")
+      if handle then
+        local output = handle:read("*a")
+        handle:close()
+        return output
+      end
+      return nil
+    end
+
+    -- Функция для извлечения VM Service URI
+    local function extract_vm_service_uri()
+      local flutter_info = get_flutter_process_info()
+      if flutter_info then
+        local uri = flutter_info:match("http://[%d%.]+:%d+/[%w%-_]+=/")
+        return uri
+      end
+      return nil
+    end
+
+    -- Функция для показа Flutter debug информации
+    local function show_flutter_debug_info()
+      local handle = io.popen(
+      "tmux capture-pane -t 1 -p | grep -E '(A Dart VM Service|Flutter DevTools|Hot reload|Hot restart)' | tail -10")
+      if handle then
+        local output = handle:read("*a")
+        handle:close()
+
+        print("=== Flutter Debug Information ===")
+        if output and output ~= "" then
+          print(output)
+
+          -- Извлекаем и показываем основную информацию
+          local vm_uri = output:match("(http://[%d%.]+:%d+/[%w%-_]+=/)")
+          local devtools_uri = output:match("(http://[%d%.]+:%d+%?uri=http://[%d%.]+:%d+/[%w%-_]+=/)")
+
+          if vm_uri then
+            print("VM Service URI: " .. vm_uri)
+          end
+          if devtools_uri then
+            print("DevTools URI: " .. devtools_uri)
+          end
+        else
+          print("Информация о Flutter процессе не найдена")
+          print("Убедитесь, что flutter run запущен во второй вкладке tmux")
+        end
+      end
+    end
+
+    -- Функция для быстрого подключения отладчика
+    local function quick_debug_attach()
+      local uri = extract_vm_service_uri()
+      if uri then
+        print("Подключаемся к: " .. uri)
+        -- Вызываем команду Flutter для подключения отладчика
+        vim.cmd("FlutterAttach")
+      else
+        print("Не удалось найти VM Service URI")
+        show_flutter_debug_info()
+      end
+    end
+
     -- Автокоманда для сохранения dart файлов
     vim.api.nvim_create_autocmd("BufWritePost", {
       pattern = "*.dart",
       callback = dart_hot_reload,
       desc = "Dart hot reload on save",
     })
-    
+
     -- Команда для ручного вызова
     vim.api.nvim_create_user_command("DartHotReload", dart_hot_reload, {
       desc = "Manual dart hot reload"
     })
-    
+
     -- Команда для просмотра окон tmux
     vim.api.nvim_create_user_command("TmuxWindows", function()
       local windows = vim.fn.system("tmux list-windows -F '#{window_index}: #{window_name} #{?window_active,(active),}'")
@@ -71,10 +134,49 @@ return {
     end, {
       desc = "Show tmux windows"
     })
-    
-    -- Keymap для быстрого вызова
+
+    -- Новые команды для работы с отладкой
+    vim.api.nvim_create_user_command("FlutterDebugInfo", show_flutter_debug_info, {
+      desc = "Show Flutter debug information from tmux"
+    })
+
+    vim.api.nvim_create_user_command("QuickDebugAttach", quick_debug_attach, {
+      desc = "Quick attach to running Flutter process"
+    })
+
+    -- Команда для открытия DevTools в браузере
+    vim.api.nvim_create_user_command("FlutterDevTools", function()
+      local flutter_info = get_flutter_process_info()
+      if flutter_info then
+        local devtools_uri = flutter_info:match("(http://[%d%.]+:%d+%?uri=http://[%d%.]+:%d+/[%w%-_]+=/)")
+        if devtools_uri then
+          -- Попытка открыть в браузере (Linux/macOS)
+          local open_cmd = vim.fn.has('mac') == 1 and 'open' or 'xdg-open'
+          vim.fn.system(open_cmd .. ' "' .. devtools_uri .. '"')
+          print("Открываем DevTools: " .. devtools_uri)
+        else
+          print("DevTools URI не найден")
+          show_flutter_debug_info()
+        end
+      end
+    end, {
+      desc = "Open Flutter DevTools in browser"
+    })
+
+    -- Keymaps
     vim.keymap.set('n', '<leader>fr', dart_hot_reload, { desc = 'Dart hot reload' })
     vim.keymap.set('n', '<leader>fi', function() send_key_to_flutter_window("'i'") end, { desc = 'Инспектор виджетов' })
     vim.keymap.set('n', '<leader>fp', function() send_key_to_flutter_window("'p'") end, { desc = 'Линии построения' })
+
+    -- Новые keymaps для отладки
+    vim.keymap.set('n', '<leader>fD', show_flutter_debug_info, { desc = 'Show Flutter debug info' })
+    vim.keymap.set('n', '<leader>fA', quick_debug_attach, { desc = 'Quick attach debugger' })
+    vim.keymap.set('n', '<leader>ft', '<cmd>FlutterDevTools<cr>', { desc = 'Open DevTools' })
+
+    -- Дополнительные горячие клавиши для flutter run
+    vim.keymap.set('n', '<leader>fR', function() send_key_to_flutter_window("'R'") end, { desc = 'Hot restart' })
+    vim.keymap.set('n', '<leader>fh', function() send_key_to_flutter_window("'h'") end, { desc = 'Help commands' })
+    vim.keymap.set('n', '<leader>fc', function() send_key_to_flutter_window("'c'") end, { desc = 'Clear screen' })
+    vim.keymap.set('n', '<leader>fd', function() send_key_to_flutter_window("'d'") end, { desc = 'Detach' })
   end,
 }
