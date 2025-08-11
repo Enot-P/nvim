14552d2c6b65e44e2613d8652b84cf1129fc6ed3
Nikita_Ptashkin <nikita.ptashckin@yandex.ru>
2025-08-11 04:07:11 +0300

    Теперь сам находит vm

--- lua/plugins/flutter_tools.lua
+++ lua/plugins/flutter_tools.lua
@@ -90,79 +90,249 @@ return {
         }
       })
 
-      -- Дополнительные функции для работы с отладкой
-      local function attach_to_running_flutter()
-        -- Пытаемся автоматически найти VM Service URI из tmux окна
-        local handle = io.popen("tmux capture-pane -t 1 -p | grep 'A Dart VM Service' | tail -1")
-        if handle then
-          local output = handle:read("*a")
-          handle:close()
+      -- Улучшенная функция поиска Flutter VM Services во всех tmux окнах
+      local function find_flutter_vm_uris()
+        local uris = {}
 
-          local uri = output:match("http://[%d%.]+:%d+/[%w%-_]+=/")
-          if uri then
-            print("Найден VM Service URI: " .. uri)
-            -- Подключаемся к отладчику через DAP
+        -- Ищем во всех tmux окнах
+        local windows_handle = io.popen("tmux list-windows -F '#{window_index}' 2>/dev/null")
+        if windows_handle then
+          local windows = windows_handle:read("*a")
+          windows_handle:close()
+
+          for window in windows:gmatch("[^\r\n]+") do
+            local capture_handle = io.popen(string.format(
+              "tmux capture-pane -t %s -p 2>/dev/null | grep 'A Dart VM Service'", window))
+            if capture_handle then
+              local output = capture_handle:read("*a")
+              capture_handle:close()
+
+              if output and output ~= "" then
+                for line in output:gmatch("[^\r\n]+") do
+                  local uri = line:match("(http://[%d%.]+:%d+/[%w%-_]+=/)")
+                  if uri then
+                    table.insert(uris, {
+                      uri = uri,
+                      window = window,
+                      line = line
+                    })
+                  end
+                end
+              end
+            end
+          end
+        end
+
+        return uris
+      end
+
+      -- Telescope интеграция для выбора VM Service
+      local function attach_with_telescope()
+        local uris = find_flutter_vm_uris()
+
+        if #uris == 0 then
+          print("No Flutter VM Service found")
+          return
+        end
+
+        if #uris == 1 then
+          local uri = uris[1].uri
+          print("Auto-connecting to: " .. uri .. " (window " .. uris[1].window .. ")")
+          require('dap').run({
+            type = 'dart',
+            request = 'attach',
+            name = 'Attach to Flutter',
+            vmServiceUri = uri,
+          })
+          return
+        end
+
+        -- Проверяем наличие telescope
+        local has_telescope, telescope = pcall(require, 'telescope')
+        if not has_telescope then
+          print("Telescope not available, falling back to manual selection")
+          print("Found multiple Flutter VM Services:")
+          for i, uri_info in ipairs(uris) do
+            print(string.format("[%d] Window %s: %s", i, uri_info.window, uri_info.uri))
+          end
+
+          local choice = tonumber(vim.fn.input("Select VM Service (1-" .. #uris .. "): "))
+          if choice and choice >= 1 and choice <= #uris then
+            local selected_uri = uris[choice].uri
+            print("Connecting to: " .. selected_uri)
             require('dap').run({
               type = 'dart',
               request = 'attach',
-              name = 'Attach to Running Flutter',
-              vmServiceUri = uri,
+              name = 'Attach to Flutter',
+              vmServiceUri = selected_uri,
             })
+          end
+          return
+        end
+
+        local pickers = require('telescope.pickers')
+        local finders = require('telescope.finders')
+        local conf = require('telescope.config').values
+        local actions = require('telescope.actions')
+        local action_state = require('telescope.actions.state')
+
+        local opts = {}
+
+        pickers.new(opts, {
+          prompt_title = "Flutter VM Services",
+          finder = finders.new_table {
+            results = uris,
+            entry_maker = function(entry)
+              return {
+                value = entry.uri,
+                display = string.format("Window %s: %s", entry.window, entry.uri),
+                ordinal = entry.uri,
+              }
+            end,
+          },
+          sorter = conf.generic_sorter(opts),
+          attach_mappings = function(prompt_bufnr, map)
+            actions.select_default:replace(function()
+              actions.close(prompt_bufnr)
+              local selection = action_state.get_selected_entry()
+              if selection then
+                print("Connecting to: " .. selection.value)
+                require('dap').run({
+                  type = 'dart',
+                  request = 'attach',
+                  name = 'Attach to Flutter (Telescope)',
+                  vmServiceUri = selection.value,
+                })
+              end
+            end)
+            return true
+          end,
+        }):find()
+      end
+
+      -- Умная функция подключения к Flutter процессу
+      local function attach_to_running_flutter()
+        local uris = find_flutter_vm_uris()
+
+        if #uris == 0 then
+          print("No Flutter VM Service found in tmux windows")
+          local manual_uri = vim.fn.input("Enter VM Service URI manually: ", "http://127.0.0.1:40499/")
+          if manual_uri and manual_uri ~= "" then
+            require('dap').run({
+              type = 'dart',
+              request = 'attach',
+              name = 'Manual Attach to Flutter',
+              vmServiceUri = manual_uri,
+            })
+          end
+          return
+        end
+
+        if #uris == 1 then
+          -- Один URI найден - подключаемся автоматически
+          local uri = uris[1].uri
+          print("Auto-connecting to: " .. uri .. " (window " .. uris[1].window .. ")")
+          require('dap').run({
+            type = 'dart',
+            request = 'attach',
+            name = 'Attach to Flutter',
+            vmServiceUri = uri,
+          })
+        else
+          -- Несколько URI - используем Telescope если доступен
+          local has_telescope = pcall(require, 'telescope')
+          if has_telescope then
+            attach_with_telescope()
           else
-            print("Не удалось найти VM Service URI автоматически")
-            local manual_uri = vim.fn.input("Введите VM Service URI: ", "http://127.0.0.1:34499/")
-            if manual_uri and manual_uri ~= "" then
+            -- Показываем простой выбор
+            print("Multiple Flutter VM Services found:")
+            for i, uri_info in ipairs(uris) do
+              print(string.format("[%d] Window %s: %s", i, uri_info.window, uri_info.uri))
+            end
+
+            local choice = tonumber(vim.fn.input("Select VM Service (1-" .. #uris .. "): "))
+            if choice and choice >= 1 and choice <= #uris then
+              local selected_uri = uris[choice].uri
+              print("Connecting to: " .. selected_uri)
               require('dap').run({
                 type = 'dart',
                 request = 'attach',
-                name = 'Attach to Running Flutter',
-                vmServiceUri = manual_uri,
+                name = 'Attach to Flutter',
+                vmServiceUri = selected_uri,
               })
+            else
+              print("Invalid selection")
             end
           end
         end
       end
 
       local function get_flutter_vm_service_info()
-        local handle = io.popen("tmux capture-pane -t 1 -p | grep -E '(A Dart VM Service|Flutter DevTools)' | tail -2")
-        if handle then
-          local output = handle:read("*a")
-          handle:close()
-          print("Flutter Debug Info:")
-          print(output)
+        local uris = find_flutter_vm_uris()
 
-          -- Показываем также активные DAP сессии
-          local dap = require('dap')
-          if dap.session() then
-            print("DAP Session: Active")
-          else
-            print("DAP Session: Not active")
-          end
+        print("=== Flutter Debug Information ===")
+
+        if #uris == 0 then
+          print("No Flutter VM Services found in tmux windows")
+          print("Make sure flutter run is started in one of the tmux windows")
         else
-          print("Не удалось получить информацию о Flutter процессе")
+          print("Found Flutter VM Services:")
+          for i, uri_info in ipairs(uris) do
+            print(string.format("  [%d] Window %s: %s", i, uri_info.window, uri_info.uri))
+          end
+        end
+
+        -- Показываем также активные DAP сессии
+        local dap = require('dap')
+        if dap.session() then
+          print("\nDAP Session: Active")
+          print("Session ID: " .. tostring(dap.session().id))
+        else
+          print("\nDAP Session: Not active")
         end
       end
 
       -- Команды для удобной работы
       vim.api.nvim_create_user_command("FlutterAttach", attach_to_running_flutter, {
-        desc = "Attach to running Flutter process"
+        desc = "Smart attach to running Flutter process"
+      })
+
+      vim.api.nvim_create_user_command("FlutterAttachTelescope", attach_with_telescope, {
+        desc = "Attach to Flutter using Telescope"
       })
 
       vim.api.nvim_create_user_command("FlutterDebugInfo", get_flutter_vm_service_info, {
         desc = "Show Flutter VM Service information"
       })
 
+      vim.api.nvim_create_user_command("FlutterFindServices", function()
+        local uris = find_flutter_vm_uris()
+        if #uris == 0 then
+          print("No Flutter VM Services found")
+        else
+          print("Found Flutter VM Services:")
+          for i, uri_info in ipairs(uris) do
+            print(string.format("  [%d] Window %s: %s", i, uri_info.window, uri_info.uri))
+          end
+        end
+      end, {
+        desc = "Find all Flutter VM Services in tmux windows"
+      })
+
       -- Keymaps для отладки
-      vim.keymap.set('n', '<leader>fa', attach_to_running_flutter, { desc = 'Attach to Flutter process' })
+      vim.keymap.set('n', '<leader>fa', attach_to_running_flutter, { desc = 'Smart attach to Flutter' })
+      vim.keymap.set('n', '<leader>fT', attach_with_telescope, { desc = 'Attach with Telescope' })
       vim.keymap.set('n', '<leader>fd', get_flutter_vm_service_info, { desc = 'Flutter debug info' })
+      vim.keymap.set('n', '<leader>fs', '<cmd>FlutterFindServices<cr>', { desc = 'Find Flutter services' })
 
       -- Стандартные DAP keymaps
-      vim.keymap.set('n', '<leader>db', require('dap').toggle_breakpoint, { desc = 'Toggle breakpoint' })
-      vim.keymap.set('n', '<leader>dc', require('dap').continue, { desc = 'Continue' })
-      vim.keymap.set('n', '<leader>dn', require('dap').step_over, { desc = 'Step over' })
-      vim.keymap.set('n', '<leader>di', require('dap').step_into, { desc = 'Step into' })
-      vim.keymap.set('n', '<leader>do', require('dap').step_out, { desc = 'Step out' })
-      vim.keymap.set('n', '<leader>dr', require('dap').repl.open, { desc = 'Open REPL' })
+      -- Настройка DAP keymaps с использованием F-клавиш
+      vim.keymap.set('n', '<F5>', require('dap').toggle_breakpoint, { desc = 'Toggle breakpoint' })
+      vim.keymap.set('n', '<F6>', require('dap').continue, { desc = 'Continue' })
+      vim.keymap.set('n', '<F7>', require('dap').step_over, { desc = 'Step over' })
+      vim.keymap.set('n', '<F8>', require('dap').step_into, { desc = 'Step into' })
+      vim.keymap.set('n', '<F9>', require('dap').step_out, { desc = 'Step out' })
+      vim.keymap.set('n', '<F10>', require('dap').repl.open, { desc = 'Open REPL' })
     end
   },
   -- Добавляем nvim-dap для отладки
@@ -206,7 +376,7 @@ return {
           request = "attach",
           name = "Attach to process",
           vmServiceUri = function()
-            return vim.fn.input('VM Service URI: ', 'http://127.0.0.1:34499/')
+            return vim.fn.input('VM Service URI: ', 'http://127.0.0.1:40499/')
           end,
         }
       }
