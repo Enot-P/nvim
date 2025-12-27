7591f4d0ced642c76dd42c5b0c1ad5d1656c3298
nikita-ptashkin <nikita.ptashckin@yandex.ru>
2025-12-27 19:35:18 +0300

    auto save buffers in all mods

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,2 +1,16 @@
 vim.g.mapleader = " "
 
+-- Сохранение по Ctrl+S во всех режимах (:update — сохраняет только при изменениях)
+vim.keymap.set("n", "<C-s>", ":update<CR>", { silent = true, desc = "Сохранить файл" })
+vim.keymap.set(
+	"i",
+	"<C-s>",
+	"<Esc>:update<CR>a",
+	{ silent = true, desc = "Сохранить и вернуться в insert" }
+)
+vim.keymap.set(
+	"v",
+	"<C-s>",
+	"<Esc>:update<CR>gv",
+	{ silent = true, desc = "Сохранить и сохранить выделение" }
+)
