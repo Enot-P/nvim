aeb7f3237623b0b49d579683a599dc4328f44c26
nikita-ptashkin <nikita.ptashckin@yandex.ru>
2025-12-28 20:49:30 +0300

    Add debugger

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,6 @@
 -- Сюда добавляются подпапки
+-- lazy.nvim автоматически импортирует все файлы из указанных папок
 return {
   { import = "plugins.theme" },
-  { import = "plugins.lsp" },
+  { import = "plugins.lsp" }, -- включает все файлы из lsp/, включая dap.lua, flutter.lua и др.
 }
--- lua/plugins/lsp/flutter.lua
+++ lua/plugins/lsp/flutter.lua
@@ -4,6 +4,33 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "stevearc/dressing.nvim", -- optional for vim.ui.select
+    "mfussenegger/nvim-dap", -- для интеграции с дебаггером
   },
-  config = true,
+  config = function()
+    require("flutter-tools").setup({
+      debugger = {
+        -- Включаем интеграцию с nvim-dap
+        enabled = true,
+        -- Остановка на исключениях (пустой список = не останавливаться на исключениях)
+        exception_breakpoints = {},
+        -- Вызывать toString() на объектах в debug views (hover, variables list)
+        -- Это может замедлить работу, но полезно для отладки
+        evaluate_to_string_in_debug_views = true,
+        -- flutter-tools автоматически регистрирует конфигурации DAP для Dart/Flutter
+        -- Можно переопределить через register_configurations, если нужны кастомные настройки
+        -- register_configurations = function(paths)
+        --   require("dap").configurations.dart = {
+        --     {
+        --       type = "dart",
+        --       request = "launch",
+        --       name = "Launch Flutter",
+        --       program = "${workspaceFolder}/lib/main.dart",
+        --       cwd = "${workspaceFolder}",
+        --     }
+        --   }
+        -- end,
+      },
+      -- Другие настройки можно добавить здесь при необходимости
+    })
+  end,
 }
