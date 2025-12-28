3fee5c4d44bcc0c4c8f14f57333f3bb81e90382a
nikita-ptashkin <nikita.ptashckin@yandex.ru>
2025-12-28 22:56:43 +0300

    transparency for all buffers

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,7 +3,7 @@ vim.opt.cursorline = true -- Подсветка текущей строки
 vim.opt.relativenumber = true -- Номерация идет относительно строки
 vim.opt.shiftwidth = 4 -- Кол-во пробелов в табуляции
 vim.opt.clipboard = "unnamedplus" -- Синхронизация для копирования
-vim.opt.autowrite = true    -- автосохранение при переключении буферов
+vim.opt.autowrite = true -- автосохранение при переключении буферов
 vim.opt.autowriteall = true -- автосохранение при выходе
 
 -- Настройка filetype для специальных файлов
--- lua/plugins/theme/tokyonight.lua
+++ lua/plugins/theme/tokyonight.lua
@@ -1,8 +1,12 @@
 local function enable_transparency()
-    vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" }) -- Для неактивных окон
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  vim.api.nvim_set_hl(0, "MsgArea", { bg = "none" })
+  vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 end
 return {
-{
+  {
     "folke/tokyonight.nvim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
