f4b983bb94534d6c5a4d244f6538621a7fcdf4a9
nikita-ptashkin <nikita.ptashckin@yandex.ru>
2025-12-30 00:39:30 +0300

    update options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,23 +1,36 @@
 vim.opt.number = true -- Отображает номер строки
 vim.opt.cursorline = true -- Подсветка текущей строки
 vim.opt.relativenumber = true -- Номерация идет относительно строки
-vim.opt.shiftwidth = 4 -- Кол-во пробелов в табуляции
 vim.opt.clipboard = "unnamedplus" -- Синхронизация для копирования
 vim.opt.autowrite = true -- автосохранение при переключении буферов
 vim.opt.autowriteall = true -- автосохранение при выходе
+vim.opt.inccommand = "split" -- включает предварительный просмотр изменений в реальном времени для команд вроде поиска (/) или замены (:s)
+vim.opt.splitright = true -- открывать новый буффер справа при vsplit
+vim.opt.splitbelow = true -- открывать новый буффер снизу при hsplit
+vim.opt.isfname:append("@-@") -- позволяет Vim воспринимать строки вроде "user@-@domain" как единое имя файла
+vim.opt.mouse = "a" -- Поддержка мыши во всех режимах
+vim.opt.scrolloff = 8 -- Для улучшения скролла
 
--- Настройка filetype для специальных файлов
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-  pattern = { "docker-compose*.yml", "docker-compose*.yaml", "*.docker-compose.yml", "*.docker-compose.yaml" },
+-- Настройка отступов
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+-- Перносить на другую строку если больше 80 символов
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
   callback = function()
-    vim.bo.filetype = "yaml"
+    vim.opt_local.textwidth = 80
+    vim.opt_local.formatoptions:append("t") -- wrap text
+    vim.opt_local.smartindent = false
   end,
 })
 
--- Настройка filetype для GraphQL файлов
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-  pattern = { "*.graphql", "*.gql" },
-  callback = function()
-    vim.bo.filetype = "graphql"
-  end,
-})
+-- Позволит сохранить undo буффер между сессиями
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
