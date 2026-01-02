4b0da2e213d0976998c49aa09bdfba99ff560b98
Nikita_Ptashkin <nikita.ptashckin@yandex.ru>
2026-01-02 05:39:01 +0300

    refactor some fles

--- lua/plugins/markdown-render.lua
+++ lua/plugins/markdown-render.lua
@@ -1,9 +1,9 @@
 return {
   {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.icons" }, -- if you use standalone mini plugins
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
     ft = { "markdown" },
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
@@ -18,5 +18,5 @@ return {
       vim.keymap.set("n", "<leader>md+", "<cmd>RenderMarkdown expand<CR>", { desc = "Markdown: Expand Margin" })
       vim.keymap.set("n", "<leader>md-", "<cmd>RenderMarkdown contract<CR>", { desc = "Markdown: Contract Margin" })
     end,
-  }
+  },
 }
