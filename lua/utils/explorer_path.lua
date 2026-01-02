e70a9f72b5cfd19c96bf515416bf19ba55259354
Nikita_Ptashkin <nikita.ptashckin@yandex.ru>
2026-01-02 03:50:22 +0300

    get correct path dir

--- lua/utils/explorer_path.lua
+++ lua/utils/explorer_path.lua
@@ -2,11 +2,29 @@ local M = {}
 
 M.save_path_from_picker = function(picker)
   local item = picker:current()
+
   if item and item.file then
-    local my_path = item.file
-    -- Используем notify, так как print иногда перехватывается пикером
-    vim.notify("Путь получен: " .. my_path)
-    return my_path
+    local full_path = item.file
+    local dir_path
+
+    -- Проверяем, является ли выбранный элемент директорией
+    -- (В Snacks Explorer у папок обычно поле file указывает на саму папку)
+    if vim.fn.isdirectory(full_path) == 1 then
+      dir_path = full_path
+    else
+      -- Если это файл, получаем путь к его родительской папке
+      dir_path = vim.fn.fnamemodify(full_path, ":p:h")
+    end
+
+    -- Сохраняем результат
+    _G.my_saved_dir = dir_path
+
+    -- Уведомляем пользователя
+    Snacks.notify.info("Папка сохранена: " .. dir_path)
+
+    return dir_path
+  else
+    Snacks.notify.error("Не удалось определить путь")
   end
 end
 
