56668f3c5feeee11143a0bf68e141f4af0f6ba15
Nikita_Ptashkin <nikita.ptashckin@yandex.ru>
2026-01-03 01:54:57 +0300

    add scroll plug

---
+++ lua/plugins/scroll.lua
@@ -0,0 +1,42 @@
+return {
+  "petertriho/nvim-scrollbar",
+  event = { "BufReadPost", "BufNewFile" }, -- Загружать при открытии буфера
+  dependencies = {
+    "lewis6991/gitsigns.nvim", -- Для git-знаков
+    "kevinhwang91/nvim-hlslens", -- Для поиска
+  },
+  config = function()
+    require("scrollbar").setup({
+      show = true,
+      handle = {
+        blend = 30,
+      },
+      marks = {
+        GitAdd = { text = "┆" },
+        GitChange = { text = "┆" },
+        GitDelete = { text = "▁" },
+        Search = { text = { "-", "=" } },
+      },
+      excluded_filetypes = {
+        "prompt",
+        "TelescopePrompt",
+        "noice",
+        "lazy",
+        "mason",
+        -- другие, если нужно
+      },
+      handlers = {
+        cursor = true, -- Показывать позицию курсора
+        diagnostic = true, -- Диагностика LSP
+        gitsigns = true, -- Включаем интеграцию с gitsigns
+        handle = true, -- Сама полоса прокрутки
+        search = true, -- Включаем интеграцию с hlslens
+        ale = false,
+      },
+    })
+
+    require("scrollbar.handlers.gitsigns").setup()
+
+    require("scrollbar.handlers.search").setup({})
+  end,
+}
