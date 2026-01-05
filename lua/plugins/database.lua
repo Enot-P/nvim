8b57c0ea8a390230e9dee2e2b607fe2c4b586b03
Nikita_Ptashkin <nikita.ptashckin@yandex.ru>
2026-01-05 07:18:38 +0300

    Database support has been added
    add formating for database and some other lang
    
    also
    
    add .sql-formatter.json for formating sql
    
    BUT NOT ADD AUTOCOMPLETE

--- lua/plugins/database.lua
+++ lua/plugins/database.lua
@@ -11,39 +11,14 @@ return {
       require("dbee").install()
     end,
     config = function()
-      require("dbee").setup( --[[optional config]])
+      local dbee = require("dbee")
+
+      dbee.setup({
+        -- Здесь можно добавить другие опции конфигурации, если нужно
+      })
+
+      -- Глобальные горячие клавиши
+      vim.keymap.set("n", "<leader>da", dbee.toggle, { desc = "Открыть dbee UI" })
     end,
-  },
-  {
-    "kndndrj/nvim-projector",
-    dependencies = {
-      -- required:
-      "MunifTanjim/nui.nvim",
-      -- optional extensions:
-      "kndndrj/projector-neotest",
-      -- dependencies of extensions:
-      "nvim-neotest/neotest",
-    },
-    config = function()
-      require("projector").setup( --[[optional config]])
-    end,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      {
-        "MattiasMTS/cmp-dbee",
-        dependencies = {
-          { "kndndrj/nvim-dbee" },
-        },
-        ft = "sql", -- optional but good to have
-        opts = {}, -- needed
-      },
-    },
-    opts = {
-      sources = {
-        { "cmp-dbee" },
-      },
-    },
   },
 }
--- lua/plugins/lsp/formating.lua
+++ lua/plugins/lsp/formating.lua
@@ -1,53 +1,46 @@
 return {
-  -- "stevearc/conform.nvim",
-  -- event = { "BufReadPre", "BufNewFile" },
-  -- config = function()
-  --   local conform = require("conform")
-  --   conform.setup({
-  --     formatters_by_ft = {
-  --       -- Dart/Flutter - использует встроенный форматтер через LSP (dartls)
-  --       -- Если нужен явный форматтер, можно раскомментировать: dart = { "dart_format" }
-  --       -- Но лучше использовать lsp_fallback = true (уже включено ниже)
-  --       -- JSON
-  --       json = { "prettier" },
-  --       jsonc = { "prettier" },
-  --       -- YAML
-  --       yaml = { "prettier" },
-  --       -- Markdown
-  --       markdown = { "prettier" },
-  --       markdown_inline = { "prettier" },
-  --       -- Kotlin
-  --       kotlin = { "ktlint" },
-  --       -- Java
-  --       java = { "google-java-format" },
-  --       -- TOML
-  --       toml = { "taplo" },
-  --       -- Docker - форматирование через LSP (dockerls) или можно не указывать
-  --       -- hadolint это линтер, не форматтер, поэтому используем lsp_fallback
-  --       -- Bash/Zsh
-  --       bash = { "shfmt" },
-  --       sh = { "shfmt" },
-  --       zsh = { "shfmt" },
-  --       -- SQL
-  --       sql = { "sql-formatter" },
-  --       -- GraphQL
-  --       graphql = { "prettier" },
-  --       -- Lua
-  --       lua = { "stylua" },
-  --     },
-  --     format_on_save = {
-  --       lsp_fallback = true,
-  --       async = false,
-  --       timeout_ms = 3000,
-  --     },
-  --   })
-  --
-  --   vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-  --     conform.format({
-  --       lsp_fallback = true,
-  --       async = false,
-  --       timeout_ms = 1000,
-  --     })
-  --   end, { desc = "Format file or range (in visual mode)" })
-  -- end,
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+    conform.setup({
+      formatters_by_ft = {
+        -- Dart/Flutter - использует встроенный форматтер через LSP (dartls)
+        -- Если нужен явный форматтер, можно раскомментировать: dart = { "dart_format" }
+        -- Но лучше использовать lsp_fallback = true (уже включено ниже)
+        -- JSON
+        json = { "prettier" },
+        jsonc = { "prettier" },
+        -- YAML
+        yaml = { "prettier" },
+        -- TOML
+        toml = { "taplo" },
+        -- SQL
+        sql = { "sql_formatter" },
+        lua = { "stylua" },
+      },
+      formatters = {
+        sql_formatter = {
+          prepend_args = {
+            "-c", -- правильный флаг
+
+            vim.fn.expand("~/.config/nvim/lua/.sql-formatter.json"),
+          },
+        },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 3000,
+      },
+    })
+
+    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      })
+    end, { desc = "Format file or range (in visual mode)" })
+  end,
 }
