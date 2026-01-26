352c3cb40c6a4c7a592eacfa0dc513937ff609e8
nikita-ptashkin <nikita.ptashckin@yandex.ru>
2026-01-27 00:32:05 +0300

    add snippets for dart

--- lua/plugins/lsp/snippets.lua
+++ lua/plugins/lsp/snippets.lua
@@ -3,57 +3,18 @@ return {
     build = "make install_jsregexp",
     config = function()
         local ls = require("luasnip")
-        local s = ls.snippet
-        local t = ls.text_node
-        local i = ls.insert_node
-        local f = ls.function_node
 
-        -- Python snippets
-        ls.add_snippets("python", {
-            s("ifmain", {
-                t({ 'if __name__ == "__main__":', '\t' }),
-                i(1, "pass"),
-            }),
-            s("print", {
-                t("print("),
-                i(1, ""),
-                t(")"),
-            }),
-        })
+        -- Загрузить только кастомные VSCode-style snippets из ~/.config/nvim/snippets
+        local snippets_path = vim.fn.expand("~/.config/nvim/snippets")
 
-        -- Lua snippets
-        ls.add_snippets("lua", {
-            s("fn", {
-                t("local function "),
-                i(1, "name"),
-                t("("),
-                i(2, ""),
-                t({ ")", "\t" }),
-                i(3, ""),
-                t({ "", "end" }),
-            }),
-            s("for", {
-                t("for "),
-                i(1, "i"),
-                t(" = "),
-                i(2, "1"),
-                t(", "),
-                i(3, "n"),
-                t({ " do", "\t" }),
-                i(4, ""),
-                t({ "", "end" }),
-            }),
-        })
+        -- Проверяем, существует ли папка со snippets
+        if vim.fn.isdirectory(snippets_path) == 1 then
+            require("luasnip.loaders.from_vscode").lazy_load({
+                paths = { snippets_path }
+            })
+        end
 
-        -- Global snippets (работают во всех файлах)
-        ls.add_snippets("all", {
-            s("todo", {
-                t("TODO: "),
-                i(1, "description"),
-            }),
-        })
-
-        -- Загрузить существующие сниппеты (если есть в папке)
+        -- Расширения для типов файлов
         ls.filetype_extend("javascript", { "html", "css" })
     end,
 }
