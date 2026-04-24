b614eeb2849375e266e82454a7e9ee58ccc13519
Enot-P <nikita.ptashckin@yandex.ru>
2026-04-24 23:38:40 +0300

    update lua conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,30 +1,33 @@
 vim.pack.add({
-	{ src = "https://github.com/stevearc/conform.nvim" },
+    { src = "https://github.com/stevearc/conform.nvim" },
 })
 
 require("conform").setup({
-	formatters_by_ft = {
-		sql = { "sql_formatter" },
-		go = { "goimports", "gofmt" },
-		python = { "ruff_format" },
-		lua = { "stylua" },
-	},
+    formatters_by_ft = {
+        sql = { "sql_formatter" },
+        go = { "goimports", "gofmt" },
+        python = { "ruff_format" },
+        lua = { "stylua" },
+    },
 
-	format_on_save = function(bufnr)
-		if vim.bo[bufnr].filetype == "sql" then
-			return { timeout_ms = 800, lsp_format = "never" }
-		end
-		return { timeout_ms = 800, lsp_format = "fallback" }
-	end,
+    format_on_save = function(bufnr)
+        if vim.bo[bufnr].filetype == "sql" then
+            return { timeout_ms = 800, lsp_format = "never" }
+        end
+        return { timeout_ms = 800, lsp_format = "fallback" }
+    end,
 
-	formatters = {
-		sql_formatter = {
-			command = "npx",
-			args = {
-				"sql-formatter",
-				"-c",
-				vim.fn.stdpath("config") .. "/.sql-formatter.json",
-			},
-		},
-	},
+    formatters = {
+        stylua = {
+            prepend_args = { "--config-path", vim.fn.stdpath("config") .. "/stylua.toml" },
+        },
+        sql_formatter = {
+            command = "npx",
+            args = {
+                "sql-formatter",
+                "-c",
+                vim.fn.stdpath("config") .. "/.sql-formatter.json",
+            },
+        },
+    },
 })
