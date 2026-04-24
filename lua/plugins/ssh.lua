e78048d5ed2bd0d4deedb2fe42d6462b5245936f
Enot-P <nikita.ptashckin@yandex.ru>
2026-04-24 21:31:44 +0300

    repair clipboard in tmux

--- lua/plugins/ssh.lua
+++ lua/plugins/ssh.lua
@@ -34,15 +34,3 @@ vim.schedule(function()
 		},
 	})
 end)
-
-vim.g.clipboard = {
-	name = "OSC 52",
-	copy = {
-		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-	},
-	paste = {
-		["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-		["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-	},
-}
