5460279d49a71972180b3141b5f8d7b4c8b66162
Enot-P <nikita.ptashckin@yandex.ru>
2026-05-15 23:07:34 +0300

    fix recording label

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,4 +2,25 @@ vim.pack.add({
   { src = "https://github.com/nvim-lualine/lualine.nvim" },
 })
 
-require("lualine").setup({})
+require("lualine").setup({
+    options = {
+        icons_enabled = true,
+        theme = "auto",
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+    },
+    sections = {
+        lualine_a = {
+            {
+                "mode",
+                fmt = function(str)
+                    local recording = vim.fn.reg_recording()
+                    if recording ~= "" then
+                        return "RECORDING @" .. recording
+                    end
+                    return str
+                end,
+            },
+        },
+    },
+})
